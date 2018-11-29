e8283031e0018f32b213357111dfa01ab8ddac23
swpuleo <swpu.leo@gmail.com>
2018-11-29 18:05:32 +0800

    添加 Remove Duplicates from Sorted List

---
+++ src/easy/RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  var cur = head;
+  if (cur === null) return null;
+  while(cur.next) {
+    if (cur.next.val === cur.val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return head;
+};
