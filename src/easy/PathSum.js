4c4373450fa3cac1821b5725c366873845846b1d
swpuleo <swpu.leo@gmail.com>
2018-12-08 15:39:56 +0800

    Add Path Sum

---
+++ src/easy/PathSum.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+  if (!root) return false;
+  if (!root.left && !root.right) {
+    return sum - root.val === 0;
+  }
+  return hasPathSum(root.left, sum - root.val) ||
+    hasPathSum(root.right, sum - root.val);
+};
