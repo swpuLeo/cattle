9377e4481dce6a2acdec330b6fa0282cbd0b0743
swpuleo <swpu.leo@gmail.com>
2018-12-30 15:18:37 +0800

    ADD Remove Linked List Elements

---
+++ src/easy/RemoveLinkedListElements.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  if (!head) return null;
+  let res = new ListNode(null);
+  res.next = head;
+  let prev = res;
+  let cur = head;
+  while (cur) {
+    if (cur.val === val) {
+      prev.next = cur.next;
+    } else {
+      prev = cur;
+    }
+    cur = cur.next;
+  }
+  return res.next;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  if (!head) return head;
+  head.next = removeElements(head.next, val);
+  return head.val === val ? head.next : head;
+};
