b503c8a49e64756a6139dd6bd34637b2aff61d22
swpuleo <swpu.leo@gmail.com>
2019-01-07 11:46:08 +0800

    ADD Implement Queue Using Stacks

---
+++ src/easy/ImplementQueueUsingStacks.js
@@ -0,0 +1,48 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+  this.data = [];
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+  this.data.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+  return this.data.shift();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+  return this.data[0];
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+  return this.data.length === 0;  
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = Object.create(MyQueue).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
