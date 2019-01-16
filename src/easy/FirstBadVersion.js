3c93c29bedf72e52458a6b1de83a08eebaee6d6a
swpuleo <swpu.leo@gmail.com>
2019-01-16 10:32:39 +0800

    ADD First Bad Version

---
+++ src/easy/FirstBadVersion.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let begin = 0
+    let end = n
+    let lastBad
+    
+    while(begin <= end) {
+      let mid = begin + Math.floor((end - begin) / 2)
+      if (isBadVersion(mid)) {
+        lastBad = mid
+        end = mid - 1
+      } else {
+        begin = mid + 1
+      }
+    }
+    
+    return lastBad
+  };
+};
