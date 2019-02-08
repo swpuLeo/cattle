41de1fd1366a355c48eebfac0526e18e1a979356
swpuleo <swpu.leo@gmail.com>
2019-02-08 16:41:11 +0800

    ADD:: Sum of Left Leaves

---
+++ src/easy/SumOfLeftLeaves.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+  if (!root) return 0;
+  if (root.left && !root.left.left && !root.left.right) {
+    return root.left.val + sumOfLeftLeaves(root.right);
+  } else {
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+  }
+};
