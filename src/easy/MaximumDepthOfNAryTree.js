57021f99f8b168955bb395ada9b799c7405e67b7
swpuLeo <swpu.leo@gmail.com>
2019-04-04 17:26:16 +0800

    ADD:: Maximum Depth of N Ary Tree

---
+++ src/easy/MaximumDepthOfNAryTree.js
@@ -0,0 +1,16 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) return 0;
+  if (!root.children.length) return 1;
+  return 1 + Math.max(...root.children.map(node => maxDepth(node)));
+};
