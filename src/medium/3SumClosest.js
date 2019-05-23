a070d37948dc4fb26eb76688d376394c9e7b4cc8
swpuleo <swpu.leo@gmail.com>
2019-05-23 16:21:21 +0800

    ADD 3Sum Closest

---
+++ src/medium/3SumClosest.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  // 先进行排序
+  nums.sort((a, b) => a - b);
+  let closest = nums[0] + nums[1] + nums[2];
+  for (let i = 0; i < nums.length - 2; i++) {
+    // 使用双指针
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const cur = nums[i] + nums[left] + nums[right];
+      if (Math.abs(cur - target) < Math.abs(closest - target)) {
+        closest = cur;
+      }
+      if (cur < target) left += 1;
+      else if (cur > target) right -= 1;
+      else return target;
+    }
+  }
+  return closest;
+};
