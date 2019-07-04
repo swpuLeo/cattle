8fc8bd5f444562f5b354c9ffbec6d519bd09cd58
swpuleo <swpu.leo@gmail.com>
2019-07-04 20:38:32 +0800

    UPDTAE:: vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,12 +7,18 @@ const getAllMdFile = dir => {
   });
 };
 
-const easyChildren = getAllMdFile('docs/solution/easy/');
-console.log(easyChildren)
+const sortFunc = (a, b) => +a.split('-')[0] - +b.split('-')[0]
+
+const easyChildren = getAllMdFile('docs/solution/easy/')
+const mediumChildren = getAllMdFile('docs/solution/medium')
+const difficultChildren =  getAllMdFile('docs/solution/difficult')
+easyChildren.sort(sortFunc)
+mediumChildren.sort(sortFunc)
+difficultChildren.sort(sortFunc)
 
 module.exports = {
   base: '/',
-  title: 'Cattle',
+  title: 'W3Fun ❤ Cattle',
   description: '使用 JavaScript 刷 LeetCode 的历程',
   head: [
     ['link', { rel: 'icon', href: 'https://w3fun-1253290453.cos.ap-chengdu.myqcloud.com/cattle/meta/cattle-16.png' }]
@@ -27,8 +33,7 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: '从这里开始', link: '/art/' },
-      { text: '周一计划', link: '/plan/' },
-      { text: '周五复盘', link: '/review/' },
+      { text: '周计划', link: '/week/' },
       { text: '文章', link: '/idea/' },
       {
         text: '题库',
@@ -36,11 +41,10 @@ module.exports = {
           //{ text: '题目索引', link: '/solution/' },
           { text: '简单', link: '/solution/easy/' },
           { text: '中等', link: '/solution/medium/' },
-          { text: '困难', link: '/solution/diffcult/' }
+          { text: '困难', link: '/solution/difficult/' }
         ]
       },
       { text: '关于', link: '/about/' },
-      { text: '更新日志', link: '/log/' },
       { text: 'GitHub', link: 'https://github.com/swpuLeo/cattle'}
     ],
     sidebarDepth: 3,
@@ -48,6 +52,7 @@ module.exports = {
       '/art/': [
         {
           title: '从这里开始（正在更新 ...）',
+          collapsable: false,
           children: [
             '',
             'array',
@@ -64,24 +69,52 @@ module.exports = {
       ],
       '/solution/easy/': [
         {
-          title: '简单题索引',
-          children: easyChildren,
+          title: '简单题索引（正在更新 ...）',
+          collapsable: false,
+          children: easyChildren
+        }
+      ],
+      '/solution/medium/': [
+        {
+          title: '简单题索引（正在更新 ...）',
+          collapsable: false,
+          children: mediumChildren
+        }
+      ],
+      '/solution/difficult/': [
+        {
+          title: '困难题索引（正在更新 ...）',
+          collapsable: false,
+          children: difficultChildren
+        }
+      ],
+      '/week/': [
+        {
+          title: '周计划',
+          collapsable: false,
+          children: [
+            '',
+            '5'
+          ]
         }
       ],
       '/plan/': [
         {
           title: '周一计划',
+          collapsable: false,
           children: [
             '',
             'monday-plan-001',
             'monday-plan-002',
-            'monday-plan-003'
+            'monday-plan-003',
+            'monday-plan-004'
           ]
         }
       ],
       '/review/': [
         {
           title: '周五复盘',
+          collapsable: false,
           children: [
             '',
             'friday-review-001',
@@ -92,5 +125,11 @@ module.exports = {
       ]
     }
   },
-  plugins: ['@vuepress/register-components']
+  plugins: ['@vuepress/register-components'],
+  markdown: {
+    config: md => {
+      md.set({ linkify: true })
+    }
+  },
+  lastUpdated: true
 }
