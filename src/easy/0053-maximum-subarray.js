67acab8b5504801896931e0682518e73dd7bdc1c
swpuleo <swpu.leo@gmail.com>
2019-07-11 23:34:47 +0800

    ADD:: 0053

---
+++ src/easy/0053-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let sum = nums[0];
+  let curSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    let cur = nums[i];
+    if (curSum <= 0) curSum = cur;
+    else curSum += cur;
+    if (sum < curSum) sum = curSum;
+  }
+  return sum;
+};
