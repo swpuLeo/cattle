535c8811dd005608b8be1ca60a501422c3069028
swpuleo <swpu.leo@gmail.com>
2019-07-11 23:36:32 +0800

    ADD:: 0048

---
+++ src/medium/0048-rotate-image.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  let n = matrix.length
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    for (let j = i; j < n - 1 - i; j++) {
+      let tmp = matrix[i][j]
+      matrix[i][j] = matrix[n - 1 - j][i]
+      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+      matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+      matrix[j][n - 1 - i] = tmp
+    }
+  }
+};
