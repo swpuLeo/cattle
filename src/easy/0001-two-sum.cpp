80cd2d7e8bd335a454576f7dd118c8ee04e1c406
vailing <vailing817@gmail.com>
2019-07-16 22:14:42 +0800

    ADD:: 0001 cpp

--- src/easy/0001-two-sum.cpp
+++ src/easy/0001-two-sum.cpp
@@ -1,21 +1,21 @@
-˼·����ȡһ�߲����ϣ��һ��Ѱ��һ�����Ѿ�����Ĺ�ϣ����Ѱ�ҵķ�ʽ��
-    ÿ�ζ����ż��������ϣ��������Ȼ���ڹ�ϣ�������Ƿ����ʣ�µ��Ǹ�������
-    ���˿��ܻ��ʣ���ѭ��������ҵĻ�ʱ�临�ӶȲ��ͱ�����
-    ���ǹ�ϣ������ʱ�����˵��o(1)�ǳ����������ͻ���㷨��������ַ���ܲ�Ϊo(1)����һ�㲻������ַ�������ͻ����
-    �����ܵ�ʱ�临�ӶȾ�Ϊo(n)����ϣ������һ���ÿռ任ȡʱ����㷨��
+/*思路：采取一边插入哈希表一边寻找一边在已经插入的哈希表中寻找的方式，
+每次都拿着即将插入哈希表的数字然后在哈希表中找是否存在剩下的那个函数。
+有人可能会问，在循环里面查找的话时间复杂度不就变高了嘛，
+但是哈希表查找时间可以说是o(1)是常数（避免冲突的算法采用链地址可能不为o(1)，但一般不用链地址来避免冲突），
+所以总的时间复杂度就为o(n)，哈希表就是一种用空间换取时间的算法。*/
 class Solution {
-public:  
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        unordered_map<int, int> hash;//����unorder_map�ٶ�Ҫ��map������ѡ�������ϣ��  
-        for(int i=0; i < nums.size();++i){
-            int another = target - nums[i];
-            if(hash.count(another)){  
-                res = vector<int>({hash[another], i});
-                return res;
-            }
-            hash[nums[i]] = i;
-        }
+public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> res;
+    unordered_map<int, int> hash;//由于unorder_map速度要比map快所以选择无序哈希表  
+    for (int i = 0; i < nums.size(); ++i) {
+      int another = target - nums[i];
+      if (hash.count(another)) {
+        res = vector<int>({ hash[another], i });
         return res;
+      }
+      hash[nums[i]] = i;
     }
+    return res;
+  }
 };
