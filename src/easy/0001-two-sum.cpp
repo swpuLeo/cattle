67204d363f1ac9a9466b1014bfc3bb9190c1652f
vailing <vailing817@gmail.com>
2019-07-16 21:43:14 +0800

    更新第一题cpp解法！

--- src/easy/0001-two-sum.cpp
+++ src/easy/0001-two-sum.cpp
@@ -1,17 +1,21 @@
+思路：采取一边插入哈希表一边寻找一边在已经插入的哈希表中寻找的方式，
+    每次都拿着即将插入哈希表的数字然后在哈希表中找是否存在剩下的那个函数。
+    有人可能会问，在循环里面查找的话时间复杂度不就变高了嘛，
+    但是哈希表查找时间可以说是o(1)是常数（避免冲突的算法采用链地址可能不为o(1)，但一般不用链地址来避免冲突），
+    所以总的时间复杂度就为o(n)，哈希表就是一种用空间换取时间的算法。
 class Solution {
-public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> res;
-    map<int, int> map;
-    for (int i = 0; i < nums.size(); i++) {
-      int cur = nums[i];
-      int diff = target - cur;
-      if (map.count(diff) != 0) {
-        res.push_back(map[diff]);
-        res.push_back(i);
+public:  
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> hash;//由于unorder_map速度要比map快所以选择无序哈希表  
+        for(int i=0; i < nums.size();++i){
+            int another = target - nums[i];
+            if(hash.count(another)){  
+                res = vector<int>({hash[another], i});
+                return res;
+            }
+            hash[nums[i]] = i;
+        }
         return res;
-      }
-      map[cur] = i;
     }
-  }
 };
