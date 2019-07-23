2a89e74846519147209a21e3867fd24c767f78a9
vailing <vailing817@gmail.com>
2019-07-23 21:25:19 +0800

    ADD::0058 cpp

---
+++ src/easy/0058-length-of-last-world.cpp
@@ -0,0 +1,11 @@
+﻿/* 思路：此题只需要逆序遍历即可，但是要注意排除掉最后的空格。*/
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		int i = s.length() - 1;
+		for (; i >= 0 && s.at(i) == ' '; i--);
+		int j = i;
+		for (; j >= 0 && s.at(j) != ' '; j--);
+		return i - j;
+	}
+};
