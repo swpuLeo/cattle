9cfda94f02ae250a3db812f68b472466ddeed0bc
vailing <vailing817@gmail.com>
2019-07-26 16:28:44 +0800

    ADD::0104 cpp

---
+++ src/easy/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,12 @@
+﻿/* 思路：此题只需要采取深度优先遍历算法（先序遍历）求出左右孩子结点最高的高度，然后返回值再加上一即可*/
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+		if (root == NULL) return 0;
+		if (root->left == NULL && root->right == NULL) return 1;
+		int l = maxDepth(root->left);
+		int r = maxDepth(root->right);
+		return (r > l ? r : l) + 1;
+
+	}
+};
