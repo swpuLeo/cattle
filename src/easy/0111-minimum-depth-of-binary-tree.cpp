316469c525a02939f777224ee3662ba3593217c0
vailing <vailing817@gmail.com>
2019-07-28 13:49:35 +0800

    ADD::0111 cpp

---
+++ src/easy/0111-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,14 @@
+﻿/* 思路：此题只需要采取深度优先遍历算法（先序遍历）求出左右孩子结点最小的高度，然后返回值再加上一即可*/
+class Solution {
+public:
+	int minDepth(TreeNode* root) {
+		if (root == NULL) return 0;
+		if (root->left == NULL && root->right == NULL) return 1;
+		if (root->left == NULL) return minDepth(root->right) + 1;
+		if (root->right == NULL) return minDepth(root->left) + 1;
+
+		int l = minDepth(root->left);
+		int r = minDepth(root->right);
+		return (l > r) ? (r + 1) : (l + 1);
+	}
+};
