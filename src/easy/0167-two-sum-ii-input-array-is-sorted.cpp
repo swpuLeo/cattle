292acbfa586241ae045a1e108e28655962cb0e5f
vailing <vailing817@gmail.com>
2019-08-03 13:12:35 +0800

    ADD::0168 cpp

--- src/easy/0167-two-sum-ii-input-array-is-sorted.cpp
+++ src/easy/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,7 +1,5 @@
-/* 思路：此题由于是一个有序数组，所以只需要从两端同时进行遍历即可，
-如果前面的元素加上后面的元素等于目标元素即找到答案；
-若之和小于目标元素，则需要增大值才行，所以前面的索引 + 1；
-反之，则过大，后面的索引 - 1；同时应该注意，我们判断之和是否为目标元素的时候可以用减法，因为可能出现越界的情况；*/
+* 思路：此题由于是一个有序数组，所以只需要从两端同时进行遍历即可，如果前面的元素加上后面的元素等于目标元素即找到答案；若之和小于目标元素，则需要增大值才行，所以前面的索引 + 1；反之，则过大，后面的索引 - 1；同时应该注意，我们判断之和是否为目标元素的时候可以用减法，因为可能出现越界的情况；
+```
 class Solution {
 public:
   vector<int> twoSum(vector<int>& numbers, int target) {
@@ -9,7 +7,7 @@ public:
     int begin = 0;
     int end = numbers.size() - 1;
     while (begin < end)
-      if (numbers[begin] == target - numbers[end])
+      if (numbers[begin] + numbers[end] == target)
       {
         res.push_back(begin + 1);
         res.push_back(end + 1);
@@ -20,3 +18,4 @@ public:
     return res;
   }
 };
+```
