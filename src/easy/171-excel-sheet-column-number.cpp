97809cf08c9c963d925d1fe2b25ec4f543b334d4
vailing <vailing817@gmail.com>
2019-08-06 16:11:59 +0800

    ADD::0171 cpp

---
+++ src/easy/171-excel-sheet-column-number.cpp
@@ -0,0 +1,11 @@
+﻿/* 思路：此题就是一个进制转化的题，将26进制（1 - 26不含0）转化为10进制数即可。*/
+class Solution {
+public:
+	int arrayPairSum(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		int sum = 0;
+		for (int i = 0; i < nums.size(); i += 2)
+			sum += nums[i];
+		return sum;
+	}
+};
