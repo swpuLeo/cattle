08ef1dce4f4a96769f691849d291a64070dea4d9
lee <tjuser@tutudeMacBook-Pro.local>
2019-09-02 16:01:38 +0800

    reponsive layout

--- docs/.vuepress/components/WeekList.vue
+++ docs/.vuepress/components/WeekList.vue
@@ -3,12 +3,17 @@
     <h1 style="margin-bottom: 32px">周计划</h1>
     <a-row :gutter="40">
       <a-col
-        :span="8"
-        v-for="(item, index) in [5, 6, 7, 8, 9, 10, 11, 12, 13].reverse()"
+        :xs="24"
+        :sm="12"
+        :lg="8"
+        v-for="(item, index) in week"
         :key="index"
         style="margin-bottom: 40px">
         <a-badge :count="index === 0 ? 'new' : ''">
-          <a-card :title="`LeetCode 第 ${item} 周`" hoverable>
+          <a-card
+            :title="`LeetCode 第 ${item} 周`"
+            hoverable
+            @click="gotoDetail(item)">
             <img
               :alt="`week-${item}`"
               :src="`https://w3fun-1253290453.cos.ap-chengdu.myqcloud.com/cattle/week/cover/${item}.jpeg`"
@@ -22,8 +27,26 @@
 </template>
 
 <script>
+const genArray = (start, end) => {
+  return Array.from(new Array(end + 1).keys()).slice(start)
+}
+
+const start = new Date('2019-07-05')
+const end = new Date()
+const diff = Math.floor((end - start) / 604800000)
+
 export default {
-  name: 'WeekList'
+  name: 'WeekList',
+  data () {
+    return {
+      week: genArray(5, 5 + diff).reverse()
+    }
+  },
+  methods: {
+    gotoDetail (index) {
+      window.location.pathname = `/week/${index}.html`
+    }
+  }
 }
 </script>
 
