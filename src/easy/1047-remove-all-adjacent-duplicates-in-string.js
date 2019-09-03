40904af5ef89b7a6c19049e8aea7b52ff41885fe
swpuLeo <swpu.leo@gmail.com>
2019-09-03 12:57:19 +0800

    ADD:: 1047

---
+++ src/easy/1047-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeDuplicates = function(S) {
+  const stack = [];
+  for (let i of S) {
+    if (i === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(i);
+    }
+  }
+  return stack.join('');
+};
